0a14e111c42ec5c02044bb7b11491b346f6d0ba2
Tracey <treycwong@gmail.com>
2023-02-26 21:56:47 +0800

    commit

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        food = request.form["food"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(food),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +22,14 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(food):
+    return """Suggest three Malaysian food based from my favourite food
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
+Food: Pasta
+Names: Mee Goreng, Laksa, Curry Mee
+Food: Chicken
+Names: Nasi Lemak, Ayam Goreng, Satay
+Food: {}
 Names:""".format(
-        animal.capitalize()
+        food.capitalize()
     )
